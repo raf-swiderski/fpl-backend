5157e6da327834a3fdbf15749bfe6f1311ecb11a
Raf Swiderski <rafswid96@gmail.com>
2022-05-05 16:42:01 +0100

    create /myteam route with req to api

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const request = require("request");
 const express = require("express");
 const app = express()
+
 require("dotenv").config();
+const axios = require('axios').default;
+
 const port = process.env.PORT
+const API = process.env.FPL_API
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,8 +13,40 @@ app.use((req, res, next) => {
 });
 
 // id = 821650
-
 // https://fpl-api-raf.herokuapp.com/?<path>
+//   /myteam?id=821650
+
+// get team
+// all the element numbers of the players. 
+// then retrieve their value.
+
+async function getApiData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+app.get('/myteam', async (req, res, next) => { 
+    
+  const id = req.query["id"]
+  let url = `https://fpl-api-raf.herokuapp.com/?path=entry/821650/event/34/picks/`;
+
+  const myTeam = await getApiData(url)
+  .then( myTeam => {
+    console.log(myTeam)
+  })
+
+})
+
+
+
+
+
+
 app.get('/', (req, res) => { 
     
     const path = req.query["path"]
