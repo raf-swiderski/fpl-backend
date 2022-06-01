baf09401005961b5456a21318b90b7a1624ee017
Raf Swiderski <rafswid96@gmail.com>
2022-06-01 19:52:28 +0100

    create route /allplayers -bugfix

--- routes/allplayers.js
+++ routes/allplayers.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const API_URL = process.env.API_URL 
 
 const { getBoostrapStaticFromApi } = require('./getApiData')
+const { addTeamNames, convertPlayerPricing } = require('../data-logic/myteamMiddleware')
 
 router.get('/', getBoostrapStaticFromApi, async (req, res, next) => {
 
