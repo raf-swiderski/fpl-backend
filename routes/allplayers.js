865789cd204433b1530fa531735fb1a332937168
Raf Swiderski <rafswid96@gmail.com>
2023-01-01 20:28:22 +0000

    adding true value to player properties

--- routes/allplayers.js
+++ routes/allplayers.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const { getBoostrapStaticFromApi } = require('./getApiData')
-const { addTeamNamesToMyTeam, formatPlayerPricing, reduceFirstNameOfPlayersToOneName, sortPlayers, addTheInTeamProperty } = require('../data-logic/myteamMiddleware')
+const { addTeamNamesToAllPlayers, formatPlayerPricing, sortPlayers, addTheInTeamProperty } = require('../data-logic/myteamMiddleware')
 
 router.get('/', getBoostrapStaticFromApi, async (req, res, next) => {
 
     var allPlayers = req.elements;
 
-    addTeamNamesToMyTeam(allPlayers, req.premTeams);
+    addTeamNamesToAllPlayers(allPlayers, req.premTeams);
     formatPlayerPricing(allPlayers, "now_cost");
     formatPlayerPricing(allPlayers, "true_value");
     sortPlayers(allPlayers, "total_points")
